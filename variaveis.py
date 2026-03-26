709bbb2bd2fc89ca53fe51f2ed5718d328307114
Eric Carlos Coelho Santos <ericcarlos951@gmail.com>
2026-03-26 11:43:03 -0300

    Add files via upload

---
+++ variaveis.py
@@ -0,0 +1,15 @@
+nome = "Eric"
+numero1 = 2
+numero2 = 7
+numero1 = 3  
+
+print(nome)
+print(numero1)
+numero1 = True
+numero2 = False
+print(numero2)
+print(numero1*numero2)
+print(numero1,numero2)
+var1 = True
+var2 = False
+print(var1 / var2)
